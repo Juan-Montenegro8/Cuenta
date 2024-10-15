eb83bc4bde388b5aec001b29b0aa1e738906780e
Juan <jj.monte2002@gmail.com>
2024-10-14 21:55:24 -0500

    Second commit

--- src/Vista/Cuenta.java
+++ src/Vista/Cuenta.java
@@ -1,21 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
 package Vista;
+import Modelo.CuentaBancaria;    
+import java.util.Scanner;
 
 /**
  *
- * @author Montenegro
+ * Crear clase CuentaBancaria
+ * Propiedades
+ *  titular
+ *  saldo
+ * Funciones
+ *  depositar
+ *  retirar
+ *  mostrar datos
  */
 public class Cuenta {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Scanner textoScanner=new Scanner(System.in);
+
+        System.out.println("Ingrese su nombre ");
+        String titular = textoScanner.next();
+        System.out.println("Ingrese su saldo bancario");
+        double saldo = textoScanner.nextDouble();
+
+        CuentaBancaria banco =new CuentaBancaria( titular , saldo);
+
+        int eleccion;
+
+        do {
+            System.out.println("Elija una opcion: ");
+            System.out.println("1: Depositar ");
+            System.out.println("2: Retirar ");
+            System.out.println("3: Mirar saldo ");        
+            System.out.println("4: Salir ");
+            eleccion = textoScanner.nextInt();
+            if(eleccion==1){
+                System.out.println("ingrese valor a depositar ");
+                double depositar = textoScanner.nextInt();
+                banco.Depositar(depositar);
+                System.out.println("Datos actualizados");
+                banco.MostrarDatos();
+            }else if(eleccion==2){
+                System.out.println("ingrese valor a retirar ");
+                double retirar = textoScanner.nextInt();
+                banco.Retirar(retirar);
+                System.out.println("Datos actualizados");
+                banco.MostrarDatos();
+            }else if(eleccion==3){
+                banco.MostrarDatos();
+            }else{
+                System.out.println("Numero ingresado no se enceuntra dentro de las opciones");
+            }
+
+        } while (eleccion!=4);
     }
 
 }
