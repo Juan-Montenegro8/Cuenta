edce31399a1232ba4d319f4fda31bcc663ef0414
Juan <jj.monte2002@gmail.com>
2024-10-14 21:57:15 -0500

    Third commit

--- src/Vista/Cuenta.java
+++ src/Vista/Cuenta.java
@@ -16,43 +16,49 @@ import java.util.Scanner;
 public class Cuenta {
     public static void main(String[] args) {
 
-        Scanner textoScanner=new Scanner(System.in);
+        try (Scanner textoScanner=new Scanner(System.in)){
+            System.out.println("Ingrese su nombre ");
+            String titular = textoScanner.next();
+            System.out.println("Ingrese su saldo bancario");
+            double saldo = textoScanner.nextDouble();
 
-        System.out.println("Ingrese su nombre ");
-        String titular = textoScanner.next();
-        System.out.println("Ingrese su saldo bancario");
-        double saldo = textoScanner.nextDouble();
+            CuentaBancaria banco =new CuentaBancaria( titular , saldo);
 
-        CuentaBancaria banco =new CuentaBancaria( titular , saldo);
+            int eleccion;
 
-        int eleccion;
+            do {
+                System.out.println("Elija una opcion: ");
+                System.out.println("1: Depositar ");
+                System.out.println("2: Retirar ");
+                System.out.println("3: Mirar saldo ");        
+                System.out.println("4: Salir ");
+                eleccion = textoScanner.nextInt();
+                if(eleccion==1){
+                    System.out.println("ingrese valor a depositar ");
+                    double depositar = textoScanner.nextInt();
+                    banco.Depositar(depositar);
+                    System.out.println("Datos actualizados");
+                    banco.MostrarDatos();
+                }else if(eleccion==2){
+                    System.out.println("ingrese valor a retirar ");
+                    double retirar = textoScanner.nextInt();
+                    banco.Retirar(retirar);
+                    System.out.println("Datos actualizados");
+                    banco.MostrarDatos();
+                }else if(eleccion==3){
+                    banco.MostrarDatos();
+                }else{
+                    System.out.println("Numero ingresado no se enceuntra dentro de las opciones");
+                }
 
-        do {
-            System.out.println("Elija una opcion: ");
-            System.out.println("1: Depositar ");
-            System.out.println("2: Retirar ");
-            System.out.println("3: Mirar saldo ");        
-            System.out.println("4: Salir ");
-            eleccion = textoScanner.nextInt();
-            if(eleccion==1){
-                System.out.println("ingrese valor a depositar ");
-                double depositar = textoScanner.nextInt();
-                banco.Depositar(depositar);
-                System.out.println("Datos actualizados");
-                banco.MostrarDatos();
-            }else if(eleccion==2){
-                System.out.println("ingrese valor a retirar ");
-                double retirar = textoScanner.nextInt();
-                banco.Retirar(retirar);
-                System.out.println("Datos actualizados");
-                banco.MostrarDatos();
-            }else if(eleccion==3){
-                banco.MostrarDatos();
-            }else{
-                System.out.println("Numero ingresado no se enceuntra dentro de las opciones");
-            }
+            } while (eleccion!=4);
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error "+e.getMessage());
+        }
 
-        } while (eleccion!=4);
+        
+
+        
     }
 
 }
