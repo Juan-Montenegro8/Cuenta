d3e3108aa8514003e180328d43bbb2eeed23b4c8
Juan <jj.monte2002@gmail.com>
2024-10-15 21:01:06 -0500

    Fourth commit

--- src/Vista/Cuenta.java
+++ src/Vista/Cuenta.java
@@ -12,6 +12,7 @@ import java.util.Scanner;
  *  depositar
  *  retirar
  *  mostrar datos
+ * 
  */
 public class Cuenta {
     public static void main(String[] args) {
@@ -34,22 +35,26 @@ public class Cuenta {
                 System.out.println("4: Salir ");
                 eleccion = textoScanner.nextInt();
                 if(eleccion==1){
-                    System.out.println("ingrese valor a depositar ");
+                    System.out.println("Ingrese valor a depositar ");
                     double depositar = textoScanner.nextInt();
                     banco.Depositar(depositar);
                     System.out.println("Datos actualizados");
                     banco.MostrarDatos();
-                }else if(eleccion==2){
-                    System.out.println("ingrese valor a retirar ");
-                    double retirar = textoScanner.nextInt();
-                    banco.Retirar(retirar);
-                    System.out.println("Datos actualizados");
-                    banco.MostrarDatos();
-                }else if(eleccion==3){
-                    banco.MostrarDatos();
                 }else{
-                    System.out.println("Numero ingresado no se enceuntra dentro de las opciones");
-                }
+                    if(eleccion==2){
+                        System.out.println("Ingrese valor a retirar ");
+                        double retirar = textoScanner.nextInt();
+                        banco.Retirar(retirar);
+                        System.out.println("Datos actualizados");
+                        banco.MostrarDatos();
+                    }else{
+                        if(eleccion==3){
+                            banco.MostrarDatos();
+                        }else{
+                            System.out.println("Numero ingresado no se enceuntra dentro de las opciones");
+                        }
+                    } 
+                } 
 
             } while (eleccion!=4);
         } catch (Exception e) {
